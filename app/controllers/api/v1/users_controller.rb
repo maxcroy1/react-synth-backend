8948d2b8a59c1e66567393ae96acd0edfad951a4
maxcroy1 <max.croy1@gmail.com>
2020-08-17 11:28:01 -0400

    Finalized auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,37 +1,38 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
-    def profile
-      render json: { user: UserSerializer.new(current_user) }, status: :accepted
-    end
+  def profile
+    render json: { user: UserSerializer.new(current_user), presets: @user.synth_settings }, status: :accepted
+  end
 
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-          @token = encode_token(user_id: @user.id)
-          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+  def create
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
+  end
+
+  def update
+      @user = User.find(user_params[:id])
+      if @user.valid?
+        @synth_setting = SynthSetting.create(gain: user_params[:synth_settings_attributes][:gain], reverb_wet: user_params[:synth_settings_attributes][:reverb_wet], reverb_decay: user_params[:synth_settings_attributes][:reverb_decay])
+        if @synth_setting.valid?
+          @preset = Preset.create(user: @user, synth_setting: @synth_setting)
+          render json: { user: UserSerializer.new(@user), jwt: @token, synth_setting: @synth_setting }, status: :created
         else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
+          render json: { error: 'failed to create preset' }, status: :not_acceptable
         end
-    end
+      else 
+        render json: { error: 'failed to find user' }, status: :not_acceptable
+      end
+  end
 
-    def update
-        @user = User.find(user_params[:id])
-        if @user.valid?
-          @synth_setting = SynthSetting.create(gain: user_params[:synth_settings_attributes][:gain], reverb_wet: user_params[:synth_settings_attributes][:reverb_wet], reverb_decay: user_params[:synth_settings_attributes][:reverb_decay])
-          if @synth_setting.valid?
-            @preset = Preset.create(user: @user, synth_setting: @synth_setting)
-            render json: { user: UserSerializer.new(@user), jwt: @token, synth_setting: @synth_setting }, status: :created
-          else
-            render json: { error: 'failed to create preset' }, status: :not_acceptable
-          end
-        else 
-          render json: { error: 'failed to find user' }, status: :not_acceptable
-        end
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:username, :password, :id, synth_settings_attributes: [:synth, :gain, :reverb_wet, :reverb_decay])
-    end
+  private
+  def user_params
+      params.require(:user).permit(:username, :password, :id, synth_settings_attributes: [:synth, :gain, :reverb_wet, :reverb_decay])
+  end
     
 end
